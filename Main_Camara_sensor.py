e0579ac71f4fa695bf767431df2f26da5d374a9c
jmonsalvo <jmonsa13@gmail.com>
2022-02-10 13:48:37 -0500

    Button added
    Se agrega codigo para manejar el inicio o cerrado de la medición a través de un boton.

--- Main_Camara_sensor.py
+++ Main_Camara_sensor.py
@@ -5,24 +5,32 @@
 # ----------------------------------------------------------------------------------------------------------------------
 # IMPORT LIBRARIES
 # ----------------------------------------------------------------------------------------------------------------------
-import RPi.GPIO as GPIO
+import cv2
 import time
+import numpy as np
+
+import picamera
+import RPi.GPIO as GPIO
+from gpiozero import Button
 
 # ----------------------------------------------------------------------------------------------------------------------
 # SETTING THE GPIO BOARD
 # ----------------------------------------------------------------------------------------------------------------------
-#GPIO Mode (BOARD / BCM)
+# GPIO Mode (BOARD / BCM)
 GPIO.setmode(GPIO.BOARD)
 
-#set GPIO Pins
+# set GPIO Pins
 TRIG = 7
 ECHO = 12
 
-#set GPIO direction (IN / OUT)
+# set GPIO direction (IN / OUT)
 GPIO.setup(TRIG, GPIO.OUT)
 GPIO.output(TRIG, False)
 
 GPIO.setup(ECHO, GPIO.IN)
+
+# Button Definition
+button = Button(37)    # Button pin definition
 # ----------------------------------------------------------------------------------------------------------------------
 # FUNCTIONS
 # ----------------------------------------------------------------------------------------------------------------------
@@ -59,9 +67,19 @@ def distance():
 if __name__ == '__main__':
     try:
         while True:
+            # Waiting for the button activation in order to continue with the program
+            button.wait_for_press()
+
+            # Definition of t_end = 10 seconds. The maximum period of time where the camera program is active
+            t_end = time.time() + 10
+
             dist = distance()
             print("Measured Distance = %.1f cm" % dist)
             time.sleep(1)
+
+            # Escape key in order to close the process and return to stand by position
+            if cv2.waitKey(20) == 27 or time.time() > t_end or button.is_pressed:
+                break
 
         # Reset by pressing CTRL + C
     except KeyboardInterrupt:
